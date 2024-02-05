e91f3273288bb73c54e0aba6aed8d6c0e7c875b7
Matthew <matthewbpo@gmail.com>
2024-02-04 21:42:58 -0500

    refactor service function to account for filters
    - also group "free-time" results by consultant_id
    - TODO: need to account for dates separately

--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from nanoid import generate
+from collections import defaultdict
 
 
 class BookingService:
@@ -25,68 +26,84 @@ class BookingService:
         id = generate()
         return self.booking_db.write({**booking_request, "id": id})
 
-    def get_free_time_for_month(self, month, filter={}):
+    def get_free_time(self, data={}):
+        filter = {}
+
+        if "month" in data:
+            filter = {**filter, "date": {"$regex": f"^{data['month']}/"}}
+
+        if "date" in data:
+            filter = {**filter, "date": data["date"]}
+
+        if "consultant_id" in data:
+            filter = {**filter, "consultant_id": data["consultant_id"]}
+
+        if "start_time" and "end_time" in data:
+            start_time = data["start_time"]
+            end_time = data["end_time"]
+            filter = {
+                **filter,
+                "start_time": {"$lte": start_time},
+                "end_time": {"$gte": end_time},
+            }
+
+        print(f"Filter: {filter}")
+
         # get consultant free time
-        filter = {**filter, "date": {"$regex": f"^{month}/"}}
-        print(filter)
+        ## TODO: group by consultant_id?
         free_time = self.ft_db.read_all(filter)
 
         # get consultant bookings
-        current_bookings = self.booking_db.read_all(filter)
-
-        available_times = self.find_free_time(free_time, current_bookings)
-        return available_times
-
-    def get_free_time(self, date, filter={}):
-        # get consultant free time
-        filter = {**filter, "date": date}
-        free_time = self.ft_db.read_all(filter)
-
-        # get consultant bookings
+        ## TODO: group by consultant_id
         current_bookings = self.booking_db.read_all(filter)
 
         available_times = self.find_free_time(free_time, current_bookings)
         return available_times
 
     def find_free_time(self, free_intervals, booked_intervals):
-        # print(f"free_interval: {free_intervals}")
-        # print(f"booked_interval: {booked_intervals}")
         result = []
 
-        for free_interval in free_intervals:
-            free_start = datetime.strptime(free_interval["start_time"], "%H:%M")
-            free_end = datetime.strptime(free_interval["end_time"], "%H:%M")
+        grouped_free_intervals = group_by_consultant_id(free_intervals)
+        grouped_booked_intervals = group_by_consultant_id(booked_intervals)
 
-            booked_times = [
-                (
-                    datetime.strptime(b["start_time"], "%H:%M"),
-                    datetime.strptime(b["end_time"], "%H:%M"),
-                )
-                for b in booked_intervals
-            ]
+        for consultant_id, consultant_free_intervals in grouped_free_intervals.items():
+            print(f"Now evaluating free time for {consultant_id}")
+            consultant_booked_intervals = grouped_booked_intervals[consultant_id]
 
-            current_time = free_start
-            for booked_start, booked_end in sorted(booked_times):
-                if current_time < booked_start:
+            for free_interval in consultant_free_intervals:
+                free_start = datetime.strptime(free_interval["start_time"], "%H:%M")
+                free_end = datetime.strptime(free_interval["end_time"], "%H:%M")
+
+                booked_times = [
+                    (
+                        datetime.strptime(b["start_time"], "%H:%M"),
+                        datetime.strptime(b["end_time"], "%H:%M"),
+                    )
+                    for b in consultant_booked_intervals
+                ]
+
+                current_time = free_start
+                for booked_start, booked_end in sorted(booked_times):
+                    if current_time < booked_start:
+                        result.append(
+                            {
+                                "date": free_interval["date"],
+                                "consultant_id": consultant_id,
+                                "start_time": current_time.strftime("%H:%M"),
+                                "end_time": booked_start.strftime("%H:%M"),
+                            }
+                        )
+                    current_time = max(current_time, booked_end)
+
+                if current_time < free_end:
                     result.append(
                         {
                             "date": free_interval["date"],
-                            "consultant_id": free_interval["consultant_id"],
+                            "consultant_id": consultant_id,
                             "start_time": current_time.strftime("%H:%M"),
-                            "end_time": booked_start.strftime("%H:%M"),
+                            "end_time": free_end.strftime("%H:%M"),
                         }
                     )
-                current_time = max(current_time, booked_end)
-
-            if current_time < free_end:
-                result.append(
-                    {
-                        "date": free_interval["date"],
-                        "consultant_id": free_interval["consultant_id"],
-                        "start_time": current_time.strftime("%H:%M"),
-                        "end_time": free_end.strftime("%H:%M"),
-                    }
-                )
 
         return result
 
@@ -104,23 +121,11 @@ class BookingService:
         return False
 
 
-# from mongo_service import MongoService
+def group_by_consultant_id(objects):
+    grouped_dict = defaultdict(list)
 
+    for obj in objects:
+        obj_id = obj["consultant_id"]
+        grouped_dict[obj_id].append(obj)
 
-# if __name__ == "__main__":
-#     booking = BookingService(
-#         MongoService(
-#             {
-#                 "database": "scheduler",
-#                 "collection": "free-time",
-#             }
-#         ),
-#         MongoService(
-#             {
-#                 "database": "scheduler",
-#                 "collection": "booking",
-#             }
-#         ),
-#     )
-
-#     print(booking.get_free_time_for_month("01"))
+    return grouped_dict
